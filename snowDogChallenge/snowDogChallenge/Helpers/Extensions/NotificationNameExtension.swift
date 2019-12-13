74614cef165d51726a50b3c2d535879a9d83469b
emir_kartal <kartal.emirr@gmail.com>
2019-12-13 22:27:55 +0300

    add notification name extension

---
+++ snowDogChallenge/snowDogChallenge/Helpers/Extensions/NotificationNameExtension.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  snowDogChallenge
+//
+//  Created by Emir Kartal on 13.12.2019.
+//  Copyright © 2019 emir. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let notifyUserDataFunc = Notification.Name("notifyUserDataFunc")
+}
